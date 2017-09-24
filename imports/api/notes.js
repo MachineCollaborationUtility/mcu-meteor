bb350270280408b28de26d69a8c0b7f60cbaa5cc
hovanem <matt.hova@autodesk.com>
2017-09-23 21:33:16 -0700

    More tests

--- imports/api/notes.js
+++ imports/api/notes.js
@@ -1,5 +1,6 @@
 import { Mongo } from 'meteor/mongo';
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
 import moment from 'moment';
 
 export const Notes = new Mongo.Collection('notes');
@@ -16,5 +17,61 @@ Meteor.methods({
       userId: this.userId,
       updatedAt: moment().valueOf(),
     });
+  },
+  'notes.remove': function (_id) {
+    // Check for user id else throw error
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    try {
+      new SimpleSchema({
+        _id: {
+          type: String,
+          min: 1,
+        },
+      }).validate({ _id });
+    } catch (ex) {
+      throw new Meteor.Error(400, ex.message);
+    }
+
+    Notes.remove({ _id, userId: this.userId });
+  },
+  'notes.update': function (_id, updates) {
+    if (!this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    try {
+      new SimpleSchema({
+        _id: {
+          type: String,
+          min: 1,
+        },
+        title: {
+          type: String,
+          optional: true,
+        },
+        body: {
+          type: String,
+          optional: true,
+        },
+      }).validate({ _id, ...updates });
+    } catch (ex) {
+      throw new Meteor.Error(400, ex.message);
+    }
+
+    Notes.update(
+      {
+        _id,
+        userId: this.userId, // Make sure update only called by note creator
+      },
+      {
+        $set: {
+          updatedAt: moment().valueOf(),
+          ...updates,
+        },
+      },
+    );
   },
 });
--- imports/api/notes.test.js
+++ imports/api/notes.test.js
@@ -1,22 +1,105 @@
-/* eslint-env mocha */
-/* eslint-disable prefer-arrow-callback */
 import { Meteor } from 'meteor/meteor';
 import expect from 'expect';
 
 import { Notes } from './notes';
 
 if (Meteor.isServer) {
-  describe('notes', function () {
-    it('should insert a new note if user is logged in', function () {
-      const userId = 'testId';
+  describe('notes', () => {
+    const note1 = {
+      _id: 'testid1',
+      title: 'My title',
+      body: 'Note body',
+      updatedAt: 0,
+      userId: 'testUserId1',
+    };
+
+    beforeEach(() => {
+      Notes.remove({});
+      Notes.insert(note1);
+    });
+    it('should insert a new note if user is logged in', () => {
+      const userId = note1.userId;
       const _id = Meteor.server.method_handlers['notes.insert'].apply({ userId });
 
       expect(Notes.findOne({ _id, userId })).toBeTruthy();
     });
 
-    it('should not insert note if not authenticated', function () {
+    it('should not insert note if not authenticated', () => {
       expect(() => {
         Meteor.server.method_handlers['notes.insert']();
+      }).toThrow();
+    });
+
+    it('should remove note', () => {
+      Meteor.server.method_handlers['notes.remove'].apply({ userId: note1.userId }, [note1._id]);
+
+      expect(Notes.findOne({ _id: note1._id })).toBeFalsy();
+    });
+
+    it('should not remove note if unauthenticated', () => {
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({}, [note1._id]);
+      }).toThrow();
+    });
+
+    it('should not remove note if invalid _id', () => {
+      expect(() => {
+        Meteor.server.method_handlers['notes.remove'].apply({ userId: note1.userId });
+      }).toThrow();
+    });
+
+    it('should update note', () => {
+      const title = 'New title!';
+
+      Meteor.server.method_handlers['notes.update'].apply(
+        {
+          userId: note1.userId,
+        },
+        [note1._id, { title }],
+      );
+
+      const note = Notes.findOne(note1._id);
+      expect(note.updatedAt).toBeGreaterThan(0);
+      expect(note).toInclude({
+        title,
+        body: note1.body,
+      });
+    });
+
+    it('should throw an error if additional params passed', () => {
+      const title = 'New title!';
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({ userId: note1.userId }, [
+          note1._id,
+          { title, foo: 'bar' },
+        ]);
+      }).toThrow();
+    });
+
+    it('should not update note, if user was not the creator', () => {
+      const title = 'This is an updated title!';
+
+      Meteor.server.method_handlers['notes.update'].apply(
+        {
+          userId: 'testid',
+        },
+        [note1._id, { title }],
+      );
+
+      const note = Notes.findOne(note1._id);
+
+      expect(note).toInclude(note1);
+    });
+
+    it('should not update note if unauthenticated', () => {
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({}, [note1._id]);
+      }).toThrow();
+    });
+
+    it('should not edit note if invalid _id', () => {
+      expect(() => {
+        Meteor.server.method_handlers['notes.update'].apply({ userId: note1.userId });
       }).toThrow();
     });
   });
